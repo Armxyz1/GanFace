d4e7eed2dd284be0572b605ad35639ef7b9ff36e
Armxyz1 <armaankpaul@gmail.com>
2025-06-20 17:41:35 +0000

    Add quiet flag to unzip command in dataset script

--- dataset.sh
+++ dataset.sh
@@ -1,7 +1,7 @@
 gdown 1Z1RqRo0_JiavaZw2yzZG6WETdZQ8qX86
 mv fairface-img-margin025-trainval.zip dataset.zip
 mkdir -p dataset
-unzip dataset.zip -d dataset
+unzip -q dataset.zip -d dataset
 gdown 1i1L3Yqwaio7YSOCj7ftgk8ZZchPG7dmH -O dataset/train.csv
 gdown 1wOdja-ezstMEp81tX1a-EYkFebev4h7D -O dataset/val.csv
 python src/preprocess.py
